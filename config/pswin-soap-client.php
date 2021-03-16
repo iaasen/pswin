8631094cfcd38a80bc90cb912f008fb20ba723a2
Ingvar Aasen <ingvar.aasen@nte.no>
2021-03-16 14:26:45 +0100

    Forgot pswin-soap-client.php

---
+++ config/pswin-soap-client.php
@@ -0,0 +1,41 @@
+<?php
+
+use Phpro\SoapClient\CodeGenerator\Assembler;
+use Phpro\SoapClient\CodeGenerator\Rules;
+use Phpro\SoapClient\CodeGenerator\Config\Config;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapOptions;
+use Phpro\SoapClient\Soap\Driver\ExtSoap\ExtSoapEngineFactory;
+
+return Config::create()
+    ->setEngine($engine = ExtSoapEngineFactory::fromOptions(
+        ExtSoapOptions::defaults('http://soap.pswin.com/?wsdl', [])
+            ->disableWsdlCache()
+    ))
+    ->setTypeDestination('temp/Type')
+    ->setTypeNamespace('Iaasen\Pswin\Soap\Type')
+    ->setClientDestination('temp')
+    ->setClientName('PswinClient')
+    ->setClientNamespace('Iaasen\Pswin\Soap')
+    ->setClassMapDestination('temp')
+    ->setClassMapName('PswinClassmap')
+    ->setClassMapNamespace('Iaasen\Pswin\Soap')
+    ->addRule(new Rules\AssembleRule(new Assembler\GetterAssembler(new Assembler\GetterAssemblerOptions())))
+    ->addRule(new Rules\AssembleRule(new Assembler\ImmutableSetterAssembler()))
+    ->addRule(
+        new Rules\IsRequestRule(
+            $engine->getMetadata(),
+            new Rules\MultiRule([
+                new Rules\AssembleRule(new Assembler\RequestAssembler()),
+                new Rules\AssembleRule(new Assembler\ConstructorAssembler(new Assembler\ConstructorAssemblerOptions())),
+            ])
+        )
+    )
+    ->addRule(
+        new Rules\IsResultRule(
+            $engine->getMetadata(),
+            new Rules\MultiRule([
+                new Rules\AssembleRule(new Assembler\ResultAssembler()),
+            ])
+        )
+    )
+;
