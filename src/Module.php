faf8a1b03eae2c8c1b1e0a187cc1e22c1f728c62
Ingvar Aasen <ingvar.aasen@nte.no>
2021-03-19 12:55:18 +0100

    Second attempt to move Module.php to correct location

---
+++ src/Module.php
@@ -0,0 +1,26 @@
+<?php
+namespace Iaasen\Pswin;
+
+use Laminas\Loader\StandardAutoloader;
+use Laminas\ModuleManager\Feature\AutoloaderProviderInterface;
+use Laminas\ModuleManager\Feature\ConfigProviderInterface;
+
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+{
+
+	public function getConfig()
+	{
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+
+	public function getAutoloaderConfig() {
+		return [
+			StandardAutoloader::class => [
+				'namespaces' => [
+					__NAMESPACE__ => __DIR__ . '/src'
+				]
+			]
+		];
+	}
+}
